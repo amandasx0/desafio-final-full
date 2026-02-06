d067770d8dcaa20aa6a4de3361c2d4b4ce59583b
amandasx0 <amandasarahcosta@gmail.com>
2026-02-06 09:29:24 -0300

    feat: adjustment

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -4,12 +4,12 @@ import styles from "./style.module.scss"
 
 const Card = ({image, altImage, title, description, clickButton, labelButton}) => {
     return (
-        <div className={styles.containerCard}>
+        <article className={styles.containerCard}>
             <img src={image} alt={altImage} className={styles.image}/>
             <h3>{title}</h3>
             <p>{description}</p>
             <Button onClick={clickButton}>{labelButton}</Button>
-        </div>
+        </article>
     )
 }
 
